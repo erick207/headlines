58793570a0f8267fb9616960dc74af7ac0875043
erick207 <erickdegreef2k@gmail.com>
2019-09-15 01:54:26 -0300

    fix EOL string

--- headlines.py
+++ headlines.py
@@ -9,7 +9,7 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              }
 
 @app.route("/")
-@app.route("/<publication>)
+@app.route("/<publication>")
 def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
