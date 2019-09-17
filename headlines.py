04c26495131e1be412794b5c49c8576f65743fc1
erick207 <erickdegreef2k@gmail.com>
2019-09-17 00:50:15 -0300

    fix syntax bug

--- headlines.py
+++ headlines.py
@@ -15,7 +15,7 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 def get_news(publication="bbc"):
     try:
         feed = feedparser.parse(RSS_FEEDS[publication])
-        return render_template("home.html", articles.feed['entries'])
+        return render_template("home.html", articles=feed['entries'])
     except:
         return "<html><body><p>publication: %s</p></body></html>" % str(publication)
     
